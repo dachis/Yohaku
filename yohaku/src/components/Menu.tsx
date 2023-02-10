60de8ede304f4fcc0d9dd8e1db2feec1e3eec83f
sudachi <takomannto1996@gmail.com>
2023-02-10 23:28:07 +0900

    メニューの実装

--- yohaku/src/components/Menu.tsx
+++ yohaku/src/components/Menu.tsx
@@ -1,26 +1,28 @@
 import Link from 'next/link'
 import React, { useEffect, useRef, useState } from 'react'
-import { ImCross } from "react-icons/im"
+import { ImCross, ImLoop2 } from "react-icons/im"
 import ReactCardFlip from 'react-card-flip'
 
 interface MenuProps {
     open: boolean
     close: () => void
+    flip: () => void
+    getIsFlip: () => boolean
 }
 
 const Menu = ({
     open = false,
-    close
+    close,
+    flip,
+    getIsFlip
 }: MenuProps) => {
     const menuRef = useRef<HTMLDivElement>(null)
-    const [isFlipped, setIsFlipped] = useState(false)
 
     const positionInRect = (x: number, y: number, top: number, left: number, width: number, height: number) => {
         return (left < x && x < left + width) && (top < y && y < top + height)
     }
 
     const clickOutsideMenu = (event: MouseEvent) => {
-        // setIsFlipped(!isFlipped)
         if (!menuRef.current) return
         const x = event.clientX
         const y = event.clientY
@@ -40,19 +42,22 @@ const Menu = ({
         <div className='fixed right-1.5 top-1'>
             {
                 open &&
-                <ReactCardFlip isFlipped={isFlipped} flipSpeedBackToFront={0.3} flipSpeedFrontToBack={0.3} flipDirection="horizontal">
-                    <div ref={menuRef} className='flex flex-col items-center bg-gray-100 w-32 h-96 pt-1 text-gray-700 shadow-xl'>
-                        <div className='w-full flex justify-end pr-1'>
-                            <ImCross className='cursor-pointer' onClick={() => close()} size={14} color={"#ffb0b0"} />
-                        </div>
-                        <Link href="/" className='text-xl mt-2 text-gray-500'>Home</Link>
+                <ReactCardFlip isFlipped={getIsFlip()} flipSpeedBackToFront={0.3} flipSpeedFrontToBack={0.3} flipDirection="horizontal">
+                    <div ref={menuRef} className='flex flex-col items-center bg-gray-100 w-32 h-96 pt-1 text-gray-700 shadow-xl font-titillium'>
+                        <ImCross className='absolute top-3 right-1 cursor-pointer hover:fill-[#ffb0b0] hover:scale-110' onClick={() => close()} size={14} />
+                        <Link href="/" className='text-xl mt-6 text-gray-500'>Home</Link>
                         <Link href="/about" className='text-xl text-gray-500'>About</Link>
                         <Link href="/concept" className='text-xl text-gray-500'>Concept</Link>
-                        <div onClick={() => setIsFlipped(!isFlipped)}>〇</div>
+                        <ImLoop2 className='absolute bottom-3 right-1 cursor-pointer hover:opacity-40 hover:scale-110' onClick={() => flip()} size={14} />
                     </div>
-                    <div className='flex flex-col items-center bg-gray-100 w-32 h-96 pt-1 text-gray-700 shadow-xl'>
-                        裏
-                        <div onClick={() => setIsFlipped(!isFlipped)}>〇</div>
+                    <div className='flex flex-col items-center justify-center bg-gray-100 w-32 h-96 pt-1 text-gray-700 shadow-xl'>
+                        <ImLoop2 className='absolute bottom-3 right-1 cursor-pointer hover:opacity-40 hover:scale-110' onClick={() => flip()} size={14} />
+                        {
+                            getIsFlip() && (
+                                <div className='rotate-90'>読書</div>
+                            )
+                        }
+                        <ImCross className='absolute top-3 right-1 cursor-pointer hover:fill-[#ffb0b0] hover:scale-110' onClick={() => close()} size={14} />
                     </div>
                 </ReactCardFlip>
             }
--- yohaku/src/components/templates/HeaderTemplate.tsx
+++ yohaku/src/components/templates/HeaderTemplate.tsx
@@ -3,25 +3,40 @@ import { useEffect, useRef, useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi"
 import Menu from "../Menu";
 
-interface HeaderTemplate {}
+interface HeaderTemplate { }
 
 const HeaderTemplate: React.FC<ScriptProps> = ({ children }) => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const [isFlipped, setIsFlipped] = useState(true)
 
   const closeMenu = () => {
     setMenuOpen(false)
+    setIsFlipped(true)
   }
 
-  useEffect(() => {
-    
-  })
+  const flip = () => {
+    setIsFlipped(!isFlipped)
+  }
+
+  const getIsFlip = () => {
+    return isFlipped
+  }
 
   return (
     <>
-      <Menu open={menuOpen} close={closeMenu}/>
+      <Menu open={menuOpen} close={closeMenu} flip={flip} getIsFlip={getIsFlip} />
       <div className="flex py-3 px-3 justify-between">
-        <span className="text-2xl text-gray-300" style={{textShadow: "1px 1px 2px #444"}}>文章.com</span>
-        <span><BiMenuAltRight className="cursor-pointer" onClick={() => setMenuOpen(true)} size={32} color={"#808080"} /></span>
+        <span className="text-2xl text-gray-300" style={{ textShadow: "1px 1px 2px #444" }}>文章.com</span>
+        <span>
+          <BiMenuAltRight
+            className="cursor-pointer"
+            onClick={() => {
+              setMenuOpen(true)
+              setTimeout(flip, 1)
+            }}
+            size={32} color={"#808080"}
+          />
+        </span>
       </div>
       <div>{children}</div>
     </>
